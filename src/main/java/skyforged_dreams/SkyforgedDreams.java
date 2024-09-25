e318d21e7499d7ea0a7096491786a6e0cdf2f1cd
Artem Ovchinnikov <arovchinnikov@icloud.com>
2024-09-25 18:45:18 +0300

    Initial commit

---
+++ src/main/java/skyforged_dreams/SkyforgedDreams.java
@@ -0,0 +1,11 @@
+package skyforged_dreams;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SkyforgedDreams implements ModInitializer {
+    public static final String MOD_ID = "skyforged-dreams";
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/skyforged_dreams/client/SkyforgedDreamsClient.java
@@ -0,0 +1,9 @@
+package skyforged_dreams.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyforgedDreamsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
